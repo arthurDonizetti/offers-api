b4b63a13b83205dfe6b2b1fc43ee430bad5ed1d1
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-22 19:16:01 -0300

    test: ensure AccountPostgreRepository retuns an account on success

---
+++ src/infra/db/sequelize/account-repository/account-repository.spec.ts
@@ -0,0 +1,28 @@
+import { SequelizeHelper as connection } from '../helpers/sequelize-helper'
+import { AccountPostgreRepository } from './account-repository'
+
+describe('Account Postgre Repository', () => {
+  beforeAll(async () => {
+    await connection.connect(process.env.DATABASE_URL)
+  })
+
+  afterAll(async () => {
+    const accountModel = (await import('../models/account-model')).default
+    await accountModel.destroy({ where: {}, truncate: true })
+    await connection.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountPostgreRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
