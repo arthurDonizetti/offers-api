1689e575ad16210ae831e258685574b608692b22
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-22 21:05:36 -0300

    refactor: connect sequelize client before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest'
 import app from '../config/app'
+import { SequelizeHelper as connection } from '../../infra/db/sequelize/helpers/sequelize-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await connection.connect(process.env.DATABASE_URL)
+  })
+
+  beforeEach(async () => {
+    const accountModel = (await import('../../infra/db/sequelize/models/account-model')).default
+    await accountModel.destroy({ where: {}, truncate: true })
+  })
+
+  afterAll(async () => {
+    const accountModel = (await import('../../infra/db/sequelize/models/account-model')).default
+    await accountModel.destroy({ where: {}, truncate: true })
+    await connection.disconnect()
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
