d0ba92dfdb167aaf22680ecdace305940c2fc4fc
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-23 20:39:43 -0300

    feat: ensure LoginController returns Unauthorized if invalid credentials are provided

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,4 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, Authentication } from './login-protocols'
+import { unauthorized } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
   constructor (
@@ -7,7 +8,10 @@ export class LoginController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email, password } = httpRequest.body
-    await this.authentication.auth({ email, password })
+    const accessToken = await this.authentication.auth({ email, password })
+    if (!accessToken) {
+      return unauthorized()
+    }
     return await new Promise(resolve => resolve(null))
   }
 }
