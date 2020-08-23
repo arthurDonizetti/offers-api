0e33384c09c8c8f9df12432b7cec8b2f838a38b2
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-23 20:54:29 -0300

    feat: ensure LoginController returns ServerError if Authentication throws

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, Authentication } from './login-protocols'
-import { unauthorized } from '../signup/signup-protocols'
+import { unauthorized, serverError } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
   constructor (
@@ -7,11 +7,15 @@ export class LoginController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    const accessToken = await this.authentication.auth({ email, password })
-    if (!accessToken) {
-      return unauthorized()
+    try {
+      const { email, password } = httpRequest.body
+      const accessToken = await this.authentication.auth({ email, password })
+      if (!accessToken) {
+        return unauthorized()
+      }
+      return await new Promise(resolve => resolve(null))
+    } catch (error) {
+      return serverError(error)
     }
-    return await new Promise(resolve => resolve(null))
   }
 }
