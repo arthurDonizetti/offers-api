12ec23e40f66668aecd4d42c3daa74dbbb2b87be
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-23 19:12:54 -0300

    test: ensure RequiredFieldValidation doesnt return if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ otherField: 'any_value' })
     expect(error).toEqual(new MissingParamError('anyField'))
   })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('anyField')
+    const result = sut.validate({ anyField: 'any_value' })
+    expect(result).toBeFalsy()
+  })
 })
