cd64150851d4f8ffabf29791362fc5d55e2b273a
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-23 20:55:32 -0300

    test: ensure LoginController returns ServerError if Authentication throws

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, Authentication, AuthenticationModel } from './login-protocols'
-import { unauthorized } from '../../helpers/http-helper'
+import { unauthorized, serverError } from '../../helpers/http-helper'
 import { LoginController } from './login-controller'
 
 interface SutTypes {
@@ -48,5 +48,12 @@ describe('Login Controller', () => {
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(unauthorized())
+  })
+
+  test('Should return ServerError if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockRejectedValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
