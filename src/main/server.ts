8dd3297d9ef77a0945d91c90a1fea79880c217b8
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-23 15:31:00 -0300

    refactor: change connection strategy

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,4 @@
-import { SequelizeHelper } from '../infra/db/sequelize/helpers/sequelize-helper'
+import app from './config/app'
 import env from './config/env'
 
-SequelizeHelper.connect(env.databaseUrl)
-  .then(async () => {
-    const app = (await import('./config/app')).default
-    app.listen(env.port, () => console.log(`Server online.\nRunning at http://localhost:${env.port}`))
-  })
+app.listen(env.port, () => console.log(`Server online.\nRunning at http://localhost:${env.port}`))
