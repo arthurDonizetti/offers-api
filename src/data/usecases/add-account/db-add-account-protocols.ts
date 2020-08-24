bfb526b0fde62ac96943cff1f48f9b4f70ab386b
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-23 23:54:57 -0300

    refactor: move Hasher to folder

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/usecases/account/add-account'
 export * from '../../../domain/models/account/account-model'
-export * from '../../../data/protocols/hasher'
+export * from '../../protocols/cryptography/hasher'
 export * from '../../protocols/db/account/add-account-repository'
--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'
-import { Hasher } from '../../../data/protocols/hasher'
+import { Hasher } from '../../../data/protocols/cryptography/hasher'
 
 export class BcryptAdapter implements Hasher {
   constructor (
