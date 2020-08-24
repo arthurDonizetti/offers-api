bf8a5e64850308a074a330d35f19e5c957c61a7b
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-24 19:52:32 -0300

    feat: add Login factory

---
+++ src/main/factories/login/login-factiory.ts
@@ -0,0 +1,23 @@
+
+import { makeLoginValidation } from './login-validation'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { AccountPostgreRepository } from '../../../infra/db/sequelize/account-repository/account-repository'
+import { SequelizeHelper as connection } from '../../../infra/db/sequelize/helpers/sequelize-helper'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter/jwt-adapter'
+
+export const makeLoginController = (): LoginController => {
+  const secret = process.env.JWT_SECRET
+  const encrypter = new JwtAdapter(secret)
+  const salt = 12
+  const hashComparer = new BcryptAdapter(salt)
+  connection.connect()
+  const accountRepository = new AccountPostgreRepository(connection)
+  const authentication = new DbAuthentication(
+    accountRepository,
+    hashComparer,
+    encrypter,
+    accountRepository)
+  return new LoginController(authentication, makeLoginValidation())
+}
