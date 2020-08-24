8cbf506855b3b21fe5ace6cc3d302ee310cff22c
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-24 16:34:53 -0300

    feat: ensure AccountPostgreRepository returns an account on loadByEmail success

--- src/infra/db/sequelize/account-repository/account-repository.ts
+++ src/infra/db/sequelize/account-repository/account-repository.ts
@@ -1,10 +1,11 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { AddAccountModel } from '../../../../domain/usecases/account/add-account'
 import { AccountModel } from '../../../../domain/models/account/account-model'
 import { AccountRepoModel } from '../models/account-model'
 import { ModelCtor, Model } from 'sequelize/types'
 
-export class AccountPostgreRepository implements AddAccountRepository {
+export class AccountPostgreRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   private readonly model: ModelCtor<Model<any, any>>
 
   constructor (private readonly connection: any) {
@@ -15,6 +16,16 @@ export class AccountPostgreRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const account = await this.model.create(accountData)
     this.connection.disconnect()
+    return {
+      id: account.getDataValue('id'),
+      name: account.getDataValue('name'),
+      email: account.getDataValue('email'),
+      password: account.getDataValue('password')
+    }
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const account = await this.model.findOne({ where: { email: email } })
     return {
       id: account.getDataValue('id'),
       name: account.getDataValue('name'),
