3911ceef7ef0ab616e00ea5bbae05c402fe4d1e6
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-24 19:28:33 -0300

    test: ensure LoginController returns bad request when Validation returns an error

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, Authentication, AuthenticationModel, Validation } from './login-protocols'
-import { unauthorized, serverError, ok } from '../../helpers/http/http-helper'
+import { unauthorized, serverError, ok, badRequest } from '../../helpers/http/http-helper'
 import { LoginController } from './login-controller'
+import { MissingParamError } from '../../../presentation/errors'
 
 interface SutTypes {
   sut: LoginController
@@ -64,7 +65,7 @@ describe('Login Controller', () => {
 
   test('Should return ServerError if Authentication throws', async () => {
     const { sut, authenticationStub } = makeSut()
-    jest.spyOn(authenticationStub, 'auth').mockRejectedValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
   })
@@ -81,5 +82,13 @@ describe('Login Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return bad request if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpRequest = makeFakeRequest()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
