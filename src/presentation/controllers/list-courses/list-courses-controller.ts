d2c4e364b6c93afcfd009f119c756b37328bbce0
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-25 18:45:41 -0300

    feat: ensure ListCoursesController calls Validation with correct values

--- src/presentation/controllers/list-courses/list-courses-controller.ts
+++ src/presentation/controllers/list-courses/list-courses-controller.ts
@@ -1,12 +1,16 @@
-import { Controller, HttpRequest, HttpResponse } from '../../../presentation/protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from '../../../presentation/protocols'
 import { ListCourses } from '../../../domain/usecases/course/list-courses'
 import { serverError, ok } from '../../helpers/http/http-helper'
 
 export class ListCoursesController implements Controller {
-  constructor (private readonly listCourses: ListCourses) {}
+  constructor (
+    private readonly listCourses: ListCourses,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const courses = await this.listCourses.list(httpRequest.body)
       return ok(courses)
     } catch (error) {
