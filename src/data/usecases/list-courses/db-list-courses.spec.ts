a6df3d4710db2ea1b0a612f04892ee676f8c60fc
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-25 20:04:25 -0300

    refactor: move fake search creation to a helper method

--- src/data/usecases/list-courses/db-list-courses.spec.ts
+++ src/data/usecases/list-courses/db-list-courses.spec.ts
@@ -2,6 +2,7 @@ import { DbListCourses } from './db-list-courses'
 import { SearchCourseModel } from '../../../domain/usecases/course/list-courses'
 import { CourseModel } from '../../../domain/models/course/course-model'
 import { ListCourseRepository } from '../../protocols/db/course/list-course-repository'
+import { HttpRequest } from 'presentation/protocols'
 
 const makeListCourseRepositoryStub = (): ListCourseRepository => {
   class ListCourseRepositoryStub implements ListCourseRepository {
@@ -26,18 +27,20 @@ const makeSut = (): SutTypes => {
   }
 }
 
+const makeFakeSearch = (): HttpRequest => ({
+  body: {
+    university: 'any_university',
+    kind: 'any_kind',
+    level: 'any_level',
+    shift: 'any_shift'
+  }
+})
+
 describe('DbListCourses UseCase', () => {
   test('Should call ListCourseRepository with correct values', async () => {
     const { sut, listCourseRepositoryStub } = makeSut()
     const listSpy = jest.spyOn(listCourseRepositoryStub, 'list')
-    const httpRequest = {
-      body: {
-        university: 'any_university',
-        kind: 'any_kind',
-        level: 'any_level',
-        shift: 'any_shift'
-      }
-    }
+    const httpRequest = makeFakeSearch()
     await sut.list(httpRequest.body)
     expect(listSpy).toHaveBeenCalledWith(httpRequest.body)
   })
@@ -46,14 +49,7 @@ describe('DbListCourses UseCase', () => {
     const { sut, listCourseRepositoryStub } = makeSut()
     jest.spyOn(listCourseRepositoryStub, 'list')
       .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const httpRequest = {
-      body: {
-        university: 'any_university',
-        kind: 'any_kind',
-        level: 'any_level',
-        shift: 'any_shift'
-      }
-    }
+    const httpRequest = makeFakeSearch()
     const promise = sut.list(httpRequest.body)
     expect(promise).rejects.toThrow()
   })
