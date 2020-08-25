36593a352a4f8b534e3f83da63001bec8f24f176
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-25 18:07:38 -0300

    feat: ensure ListCoursesController returns ok if valid values are provided

--- src/presentation/controllers/list-courses/list-courses-controller.ts
+++ src/presentation/controllers/list-courses/list-courses-controller.ts
@@ -1,14 +1,14 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../presentation/protocols'
 import { ListCourses } from '../../../domain/usecases/course/list-courses'
-import { serverError } from '../../helpers/http/http-helper'
+import { serverError, ok } from '../../helpers/http/http-helper'
 
 export class ListCoursesController implements Controller {
   constructor (private readonly listCourses: ListCourses) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.listCourses.list(httpRequest.body)
-      return await new Promise(resolve => resolve(null))
+      const courses = await this.listCourses.list(httpRequest.body)
+      return ok(courses)
     } catch (error) {
       return serverError(error)
     }
