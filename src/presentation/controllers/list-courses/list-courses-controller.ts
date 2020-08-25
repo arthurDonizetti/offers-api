565a374dbeb9bc75e5a4f4a6eb4ea07606c4930d
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-25 17:45:32 -0300

    feat: ensure ListCoursesController returns an server error if ListCourses throws

--- src/presentation/controllers/list-courses/list-courses-controller.ts
+++ src/presentation/controllers/list-courses/list-courses-controller.ts
@@ -1,11 +1,16 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../presentation/protocols'
 import { ListCourses } from '../../../domain/usecases/course/list-courses'
+import { serverError } from '../../helpers/http/http-helper'
 
 export class ListCoursesController implements Controller {
   constructor (private readonly listCourses: ListCourses) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.listCourses.list(httpRequest.body)
-    return await new Promise(resolve => resolve(null))
+    try {
+      await this.listCourses.list(httpRequest.body)
+      return await new Promise(resolve => resolve(null))
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
