48db96c0dca0707de98b333ce2fdcb8a679d9fb8
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-26 23:13:43 -0300

    test: ensure ContextContainsValueValidation dont return if validation succeeds

--- src/presentation/helpers/validators/context-contains-value-validation.spec.ts
+++ src/presentation/helpers/validators/context-contains-value-validation.spec.ts
@@ -13,4 +13,12 @@ describe('ContextContainsValue Validation', () => {
     const error = sut.validate({ anyField: 'other_value' })
     expect(error).toEqual(new ValueOutOfContextError(fieldName, context))
   })
+
+  test('Should not return if validation succeeds', () => {
+    const fieldName = 'anyField'
+    const context = ['first_value', 'second_value', 'third_value']
+    const sut = makeSut(fieldName, context)
+    const error = sut.validate({ anyField: 'first_value' })
+    expect(error).toBeFalsy()
+  })
 })
