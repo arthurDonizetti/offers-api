3b8248284f196397783e877241407b0fd5cb3cf7
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-27 02:39:50 -0300

    feat: ensure ListCoursesValidation calls OptionalFieldsValidations

--- src/main/factories/list-courses/list-courses-validation.ts
+++ src/main/factories/list-courses/list-courses-validation.ts
@@ -1,11 +1,15 @@
 import {
   ValidationComposite,
   Validation,
+  OptionalFieldValidation,
   ContextContainsValueValidation
 } from '../../../presentation/helpers/validators'
 
 export const makeListCoursesValidation = (): ValidationComposite => {
   const validations: Validation[] = []
+  validations.push(new OptionalFieldValidation('kind'))
+  validations.push(new OptionalFieldValidation('level'))
+  validations.push(new OptionalFieldValidation('shift'))
   validations.push(new ContextContainsValueValidation('kind', ['Presencial', 'EaD']))
   validations.push(new ContextContainsValueValidation('level', ['Bacharelado', 'Tecnólogo', 'Licenciatura']))
   validations.push(new ContextContainsValueValidation('shift', ['Manhã', 'Noite', 'Virtual']))
