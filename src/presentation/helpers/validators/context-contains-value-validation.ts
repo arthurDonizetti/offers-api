ee56eff0bb83ecc7f3eeed2ec564ecac64994ec9
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-27 00:17:59 -0300

    fix: change normalizeString location

--- src/presentation/helpers/validators/context-contains-value-validation.ts
+++ src/presentation/helpers/validators/context-contains-value-validation.ts
@@ -1,6 +1,11 @@
 import { Validation } from '../../protocols'
 import { ValueOutOfContextError } from '../../errors/value-out-of-context-error'
 
+// https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+const normalizeString = (value: string): string => {
+  return value.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+}
+
 export class ContextContainsValueValidation implements Validation {
   constructor (
     private readonly fieldName: string,
@@ -9,15 +14,10 @@ export class ContextContainsValueValidation implements Validation {
 
   validate (input: any): Error {
     const validValue = this.contextValues.filter(acceptedValue =>
-      this.normalizeString(acceptedValue.toLowerCase()) === this.normalizeString(input[this.fieldName].toLowerCase()))
+      normalizeString(acceptedValue.toLowerCase()) === normalizeString(input[this.fieldName].toLowerCase()))
 
     if (validValue.length === 0) {
       return new ValueOutOfContextError(this.fieldName, this.contextValues)
     }
-  }
-
-  // https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
-  private readonly normalizeString = (value: string): string => {
-    return value.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
   }
 }
