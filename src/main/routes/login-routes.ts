57ac82feb32eae80d3727bb2bde44596d4c672f6
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-27 01:18:03 -0300

    fix: change LoginFactory filename

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeSignUpController } from '../factories/signup/signup-factory'
-import { makeLoginController } from '../factories/login/login-factiory'
+import { makeLoginController } from '../factories/login/login-factory'
 
 export default async (router: Router): Promise<void> => {
   router.post('/login', adaptRoute(await makeLoginController()))
