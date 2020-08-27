28ce4d5fe01195220ab4332b83e19b8e4785a05a
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-27 14:12:25 -0300

    feat: ensure ListOffersController calls Validation with correct values

--- src/presentation/controllers/list-offers/list-offers-controller.ts
+++ src/presentation/controllers/list-offers/list-offers-controller.ts
@@ -1,14 +1,16 @@
-import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from '../../protocols'
 import { ListOffers } from '../../../domain/usecases/offer/list-offers'
-import { serverError, ok } from '../signup/signup-protocols'
+import { serverError, ok } from '../../helpers/http/http-helper'
 
 export class ListOffersController implements Controller {
   constructor (
-    private readonly listOffers: ListOffers
+    private readonly listOffers: ListOffers,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const offers = await this.listOffers.list(httpRequest.body)
       return ok(offers)
     } catch (error) {
