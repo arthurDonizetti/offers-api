f890e82e480f8e370a6fec853380d4a29f02da18
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-27 13:30:09 -0300

    refactor: move fake request to a helper method

--- src/presentation/controllers/list-offers/list-offers-controller.spec.ts
+++ src/presentation/controllers/list-offers/list-offers-controller.spec.ts
@@ -1,6 +1,7 @@
 import { ListOffersController } from './list-offers-controller'
 import { ListOffers, SearchOfferModel } from '../../../domain/usecases/offer/list-offers'
 import { OfferModel } from '../../../domain/models/offer/offer-model'
+import { HttpRequest } from '../login/login-protocols'
 
 const makeListOffersStub = (): ListOffers => {
   class ListOffersStub implements ListOffers {
@@ -25,23 +26,25 @@ const makeSut = (): SutTypes => {
   }
 }
 
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    university: 'any_university',
+    course: 'any_course',
+    city: 'any_city',
+    kind: 'any_kind',
+    level: 'any_level',
+    shift: 'any_shift',
+    order: {
+      priceWithDiscount: 'ASC'
+    }
+  }
+})
+
 describe('ListOffers Controller', () => {
   test('Should call ListOffers with correct values', async () => {
     const { sut, listOffersStub } = makeSut()
     const listSpy = jest.spyOn(listOffersStub, 'list')
-    const httpRequest = {
-      body: {
-        university: 'any_university',
-        course: 'any_course',
-        city: 'any_city',
-        kind: 'any_kind',
-        level: 'any_level',
-        shift: 'any_shift',
-        order: {
-          priceWithDiscount: 'ASC'
-        }
-      }
-    }
+    const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(listSpy).toHaveBeenCalledWith(httpRequest.body)
   })
