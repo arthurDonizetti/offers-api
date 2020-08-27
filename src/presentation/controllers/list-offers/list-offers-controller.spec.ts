9cbdbaed896b7cdb6a6c8be76872accd74afa594
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-27 14:26:24 -0300

    test: ensure ListOfferController returns a bad request if Validation returns an error

--- src/presentation/controllers/list-offers/list-offers-controller.spec.ts
+++ src/presentation/controllers/list-offers/list-offers-controller.spec.ts
@@ -2,7 +2,8 @@ import { ListOffersController } from './list-offers-controller'
 import { ListOffers, SearchOfferModel } from '../../../domain/usecases/offer/list-offers'
 import { OfferModel } from '../../../domain/models/offer/offer-model'
 import { HttpRequest, Validation } from '../../protocols'
-import { serverError, ok } from '../../helpers/http/http-helper'
+import { serverError, ok, badRequest } from '../../helpers/http/http-helper'
+import { InvalidParamError } from '../../errors'
 
 const makeValidationStub = (): Validation => {
   class ValidationStub implements Validation {
@@ -82,5 +83,12 @@ describe('ListOffers Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return bad request if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new InvalidParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('any_field')))
   })
 })
