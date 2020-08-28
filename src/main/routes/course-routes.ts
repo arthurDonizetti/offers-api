e0742d7ab83feaa599d76c43b5d3cc91a0b71dee
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-28 01:24:52 -0300

    feat: ensure /offers returns ok on success

--- src/main/routes/course-routes.ts
+++ src/main/routes/course-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeListCoursesController } from '../factories/list-courses/list-courses-factory'
+import { makeListOffersController } from '../factories/list-offers/list-offers-factory'
 
 export default async (router: Router): Promise<void> => {
   router.post('/course/list', adaptRoute(await makeListCoursesController()))
+    .post('/course/offers', adaptRoute(await makeListOffersController()))
 }
