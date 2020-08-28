82039f0fdb23845d68d45297789508511d2b63f7
Arthur Donizetti <arthur.donizetti@gmail.com>
2020-08-28 03:33:59 -0300

    chore: add database environment

--- database/config/config.js
+++ database/config/config.js
@@ -1,6 +1,17 @@
 require('dotenv').config()
 
 module.exports = {
+  "production": {
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
+    "dialect": process.env.DATABASE_DIALECT,
+    "define": {
+      "timestamps": true,
+      "underscored": true
+    }
+  },
   "development": {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
